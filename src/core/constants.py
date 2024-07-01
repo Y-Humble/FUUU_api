ccc62e7b19f6fcd82d1179c05809e6cf3230a894
Y-Humble <np.nope.q@gmail.com>
2024-07-01 21:34:03 +0300

    create: base constants

---
+++ src/core/constants.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+class Const:
+    __slots__ = ()
+    BASE_DIR: Path = Path(__file__).parent.parent
+    NAMING_CONVENTION: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
