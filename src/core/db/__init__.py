d98582975a2c5846c3edb7e915af85dcf50c6463
Y-Humble <np.nope.q@gmail.com>
2024-07-06 16:33:24 +0300

    feat: create SQlAlchemy repository

--- src/core/db/__init__.py
+++ src/core/db/__init__.py
@@ -2,7 +2,9 @@ __all__ = (
     "Base",
     "DBSidekick",
     "db_sidekick",
+    "SQlAlchemyRepo",
 )
 
 from .base import Base
+from .repositories import SQlAlchemyRepo
 from .sidekick import DBSidekick, db_sidekick
