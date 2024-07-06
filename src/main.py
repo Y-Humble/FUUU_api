92f080e5bfdfb26abe72d2ebf77b97a1bde7cbd2
Y-Humble <np.nope.q@gmail.com>
2024-07-06 17:35:30 +0300

    feat: include user router

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import ORJSONResponse
 
 from core.config import settings
 from core.lifespan import lifespan
+from user import user_router
 
 
 def app() -> FastAPI:
     """Set functionality for API"""
 
     app: FastAPI = FastAPI(
+        default_response_class=ORJSONResponse,
         title=settings.run.app_title,
         lifespan=lifespan,
         settings=settings,
@@ -23,6 +26,8 @@ def app() -> FastAPI:
         allow_headers=settings.cors.headers,
     )
 
+    app.include_router(router=user_router)
+
     return app
 
 
@@ -33,5 +38,5 @@ if __name__ == "__main__":
         port=settings.run.port,
         reload=True,
         factory=True,
-        log_level=settings.run.log_level
+        log_level=settings.run.log_level,
     )
--- src/user/__init__.py
+++ src/user/__init__.py
@@ -1,6 +1,8 @@
 __all__ = (
     "User",
     "Status",
+    "user_router",
 )
 
 from .models import User, Status
+from .routers import router as user_router
