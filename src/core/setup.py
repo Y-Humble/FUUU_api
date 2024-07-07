7385a88ecd2099e0933b5c4ae1ff261154fb36b8
Y-Humble <np.nope.q@gmail.com>
2024-07-07 10:45:11 +0300

    fix: move app function to setup

---
+++ src/core/setup.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.core.config import settings
+from src.core.lifespan import lifespan
+from src.user import user_router
+
+
+def setup_app() -> FastAPI:
+    """Set functionality for API"""
+
+    app: FastAPI = FastAPI(
+        default_response_class=ORJSONResponse,
+        title=settings.run.app_title,
+        lifespan=lifespan,
+        settings=settings,
+    )
+    app.add_middleware(
+        middleware_class=CORSMiddleware,
+        allow_origins=settings.cors.origins,
+        allow_credentials=True,
+        allow_methods=settings.cors.methods,
+        allow_headers=settings.cors.headers,
+    )
+
+    app.include_router(router=user_router)
+
+    return app
--- src/main.py
+++ src/main.py
@@ -1,34 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import ORJSONResponse
 
-from core.config import settings
-from core.lifespan import lifespan
-from user import user_router
+from src.core.config import settings
+from src.core.setup import setup_app
 
-
-def app() -> FastAPI:
-    """Set functionality for API"""
-
-    app: FastAPI = FastAPI(
-        default_response_class=ORJSONResponse,
-        title=settings.run.app_title,
-        lifespan=lifespan,
-        settings=settings,
-    )
-
-    app.add_middleware(
-        middleware_class=CORSMiddleware,
-        allow_origins=settings.cors.origins,
-        allow_credentials=True,
-        allow_methods=settings.cors.methods,
-        allow_headers=settings.cors.headers,
-    )
-
-    app.include_router(router=user_router)
-
-    return app
+app: FastAPI = setup_app()
 
 
 if __name__ == "__main__":
@@ -37,6 +13,5 @@ if __name__ == "__main__":
         host=settings.run.host,
         port=settings.run.port,
         reload=True,
-        factory=True,
         log_level=settings.run.log_level,
     )
